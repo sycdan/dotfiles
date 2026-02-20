08b9d54d764855b58dfe7bccc591f2eb539c8152
Dan Stace <dstace@gmail.com>
2026-02-20 00:14:17 -0500

    todo

--- project/workon/handler.py
+++ project/workon/handler.py
@@ -11,4 +11,8 @@ ACTION_DIR = Path(__file__).parent
 def handle(command: Workon) -> Workon.Result:
   logger.debug(f"Handling {command=}")
   logger.error(f"Logic for the Workon command must be defined in {ACTION_DIR.as_posix()}/{Path(__file__).name}")
+  # TODO
+  """
+   wsl --import projectname C:\wsl\projectname C:\wsl-images\ubuntu24_2026-02-19.tar
+  """
   return Workon.Result()
